0d6df428c1e6975c6338bf2bc3246defa7a1a1f9
Oguzhan Demiraslan <oguzhandemiraslan@gmail.com>
2023-01-22 23:20:20 -0500

    add solution 7

---
+++ basic-algorithms/7)repeat-a-string.js
@@ -0,0 +1,14 @@
+//Repeat a String Repeat a String
+//Repeat a given string str (first argument) for num times (second argument). Return an empty string if num is not a positive number. For the purpose of this challenge, do not use the built-in .repeat() method.
+
+function repeatStringNumTimes(str, num) {
+  if (num > 0) {
+    let newString = "";
+    for (let i = 0; i < num; i++) {
+      newString += str;
+    }
+    return newString;
+  } else {
+    return "";
+  }
+}
